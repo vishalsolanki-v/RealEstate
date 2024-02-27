e37e032b4a84f62aa08fc8b46dddf2b4901191e0
vishalsolanki-v <vishalthakur2463@gmail.com>
2024-02-27 23:21:00 +0530

    first commit

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -6,6 +6,7 @@ const Banner = () => {
     <>
     <div className='w-full overflow-hidden h-full px-2 pt-8'>
         <Image src="/assets/images/banner1.jpg" alt="banner1"  className='w-full h-full box-border object-cover' width={10000} height={10000}/>
+    
     </div>
     </>
   )
